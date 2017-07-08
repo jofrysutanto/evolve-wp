7746a2b0b65568a50b80c8a72e280fdb4f4b8878
jofrysutanto <jofrysutanto@gmail.com>
2017-07-08 14:55:19 +1000

    Updating composer requirements

---
+++ config/queue.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Queue Driver
+    |--------------------------------------------------------------------------
+    |
+    | The queue API supports a variety of back-ends via an unified
+    | API, giving you convenient access to each back-end using the same
+    | syntax for each one. Here you may set the default queue driver.
+    |
+    | Supported: "sqs"
+    |
+    */
+
+    'default' => env('QUEUE_DRIVER', 'sqs'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Queue Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the connection information for each server that
+    | is used by your application. A default configuration has been added
+    | for each back-end shipped with Laravel. You are free to add more.
+    |
+    */
+
+    'connections' => [
+
+        'sqs' => [
+            'key'    => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'queue'  => env('SQS_QUEUE_URL'),
+            'region' => 'ap-southeast-2',
+        ],
+
+    ],
+
+];
