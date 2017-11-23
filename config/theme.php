de886046310891fe1aa5825ca2bd2e1dabb8cf47
Jofry Sutanto <jofrysutanto@gmail.com>
2017-11-23 14:12:35 +1100

    Adding default theme announcement feed url to `config/theme.php`

--- config/theme.php
+++ config/theme.php
@@ -1,6 +1,8 @@
 <?php
 return [
 
+    'announcement' => 'https://trueagency.com.au/dash-announcement.php',
+
     'image-sizes' => [
         'full-width' => [1920, 999999],
         'half-width' => [960,  9999],
