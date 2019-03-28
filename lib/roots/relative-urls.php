19ef89ae2646465ce2dcf25e6c9fb779c60772d2
Jofry <jofry@trueagency.com.au>
2019-03-28 14:46:16 +1100

    Fix to missing composer dependencies. Fix wp-cli compat.

--- lib/roots/relative-urls.php
+++ lib/roots/relative-urls.php
@@ -10,24 +10,30 @@
  *
  * @author Scott Walkinshaw <scott.walkinshaw@gmail.com>
  */
-function roots_root_relative_url($input) {
-  preg_match('|https?://([^/]+)(/.*)|i', $input, $matches);
+function roots_root_relative_url($input)
+{
+    preg_match('|https?://([^/]+)(/.*)|i', $input, $matches);
 
-  if (!isset($matches[1]) || !isset($matches[2])) {
-    return $input; 
-  } elseif (($matches[1] === $_SERVER['SERVER_NAME']) || $matches[1] === $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT']) {
-    return wp_make_link_relative($input);
-  } else {
-    return $input;
-  }
+    if (!isset($_SERVER['SERVER_NAME'])) {
+        return $input;
+    }
+
+    if (!isset($matches[1]) || !isset($matches[2])) {
+        return $input;
+    } elseif (($matches[1] === $_SERVER['SERVER_NAME']) || $matches[1] === $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT']) {
+        return wp_make_link_relative($input);
+    } else {
+        return $input;
+    }
 }
 
-function roots_enable_root_relative_urls() {
-  return !(is_admin() || in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'))) && current_theme_supports('root-relative-urls');
+function roots_enable_root_relative_urls()
+{
+    return !(is_admin() || in_array($GLOBALS['pagenow'], ['wp-login.php', 'wp-register.php'])) && current_theme_supports('root-relative-urls');
 }
 
 if (roots_enable_root_relative_urls()) {
-  $root_rel_filters = array(
+    $root_rel_filters = [
     'bloginfo_url',
     'the_permalink',
     'wp_list_pages',
@@ -44,7 +50,7 @@ if (roots_enable_root_relative_urls()) {
     'the_author_posts_link',
     'script_loader_src',
     'style_loader_src'
-  );
+  ];
 
-  add_filters($root_rel_filters, 'roots_root_relative_url');
+    add_filters($root_rel_filters, 'roots_root_relative_url');
 }
