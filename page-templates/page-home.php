2a0b7bc9a97abcab7ea43c891cf3b9a1ddce8a60
Jofry <jofry@trueagency.com.au>
2019-03-29 11:27:53 +1100

    Minor tweaks

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -10,6 +10,12 @@
         position: fixed;
         min-height: 100vh;
         width: 100%;
+        background-color: #393939;
+        color: #fff;
+        background: #232526;  /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #414345, #232526);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to right, #414345, #232526); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
     }
     .welcome {
         position: absolute;
@@ -18,15 +24,25 @@
         padding-left: 40px; padding-right: 40px;
         transform: translate(0, -50%);
     }
+    .welcome-logo {
+        position: relative;
+        display: block;
+        width: 90px;
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+        margin-bottom: 1rem;
+        padding-bottom: 1rem;
+    }
     h1 {
         font-family: "Helvetica Neue", "Arial";
         font-size: 64px;
-        font-weight: 300;
+        font-weight: 600;
         text-align: center;
     }
 </style>
 
 <div class="welcome">
-    <img src="<?= asset('img/common/true-logo-footer-black.png') ?>" alt="True" class="center-block img-fluid">
+    <img class="welcome-logo" src="<?= asset('img/common/true-logo-white.svg') ?>" alt="True" class="img-fluid">
     <h1>Build awesome site.</h1>
 </div>
