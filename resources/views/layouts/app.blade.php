d3ad83effdb1a25120557cc287d25074e61db530
Jofry <jofry@trueagency.com.au>
2019-05-15 22:47:50 +1000

    Remove default container on layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,10 @@
   <body @php body_class() @endphp>
     @php do_action('get_header') @endphp
     @include('partials.header')
-    <div class="wrap container" role="document">
-      <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>
-      </div>
+    <div class="wrap" role="document">
+      <main class="main">
+        @yield('content')
+      </main>
     </div>
     @php do_action('get_footer') @endphp
     @include('partials.footer')
