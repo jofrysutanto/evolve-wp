c28c3e771aeb09ab35c63769619bec2be91740c4
Jofry Sutanto <jofrysutanto@gmail.com>
2019-10-23 20:27:43 +1100

    ACF to home by default

--- src/Controllers/PageHome.php
+++ src/Controllers/PageHome.php
@@ -6,10 +6,11 @@ use Sober\Controller\Controller;
 
 class PageHome extends Controller
 {
-    
+    // Pass on all fields from Advanced Custom Fields to the view
+    protected $acf = true;
+
     public function welcome()
     {
         return "Let's get started!";
     }
-
 }
