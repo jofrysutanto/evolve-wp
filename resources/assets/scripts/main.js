7ee764af3a18e447eaee9b505b4a4f925609a1a9
Jofry Sutanto <jofrysutanto@gmail.com>
2019-11-14 17:43:52 +1100

    Minor eslint fix

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -1,22 +1,22 @@
 // Import polyfills for older browsers
-import "./_polyfills";
+import './_polyfills'
 
 // Import external dependencies
-import 'jquery';
+import 'jquery'
 
 // Import everything from autoload
 import './autoload/**/*'
 
 // import local dependencies
-import Detection from './util/Detection';
-import Router from './util/Router';
-import common from './routes/common';
-import home from './routes/home';
-import aboutUs from './routes/about';
+import Detection from './util/Detection'
+import Router from './util/Router'
+import common from './routes/common'
+import home from './routes/home'
+import aboutUs from './routes/about'
 
 // Detect browser supports
 // which adds special css class to body
-window._detect = new Detection({ detect: ['ie11', 'edge']});
+window._detect = new Detection({ detect: ['ie11', 'edge']})
 window._detect.init()
 
 /** Populate Router instance with DOM routes */
@@ -27,7 +27,7 @@ const routes = new Router({
   home,
   // About Us page, note the change from about-us to aboutUs.
   aboutUs,
-});
+})
 
 // Load Events
-jQuery(document).ready(() => routes.loadEvents());
+jQuery(document).ready(() => routes.loadEvents())
